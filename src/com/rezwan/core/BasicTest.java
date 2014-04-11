1f322b4544786da76682e21509f1eaa788d4c1ec
rezwanabir <rezwanabir@192.168.0.11>
2014-04-10 20:18:06 -0400

    add annotation custom

--- src/com/rezwan/core/BasicTest.java
+++ src/com/rezwan/core/BasicTest.java
@@ -1,8 +1,32 @@
 package com.rezwan.core;
 
-public class BasicTest {
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class BasicTest { 
+	@BasicAnnotation
 	public static void main(String [] args) {
-		Person p =new Person("", "", ""); 
-		p.myMethod1(); 
+
+
+		Class<BasicTest> object=BasicTest.class; 
+		
+		if ( object.isAnnotationPresent( BasicAnnotation.class ) ) {
+			Annotation annotation=object.getAnnotation( BasicAnnotation.class );
+			BasicAnnotation basicAnnotation=(BasicAnnotation) annotation; 
+			
+			System.out.println(basicAnnotation.id()); 
+		}
+		
+		
+		
+		for ( Method method : object.getDeclaredMethods() ) {
+			if ( method.isAnnotationPresent( BasicAnnotation.class ) ) {
+				Annotation annotation=method.getAnnotation(BasicAnnotation.class);
+				BasicAnnotation basicAnnotation=(BasicAnnotation) annotation; 
+				System.out.println(basicAnnotation.id());
+			}
+		}
+ 		
+		
 	}
 }
